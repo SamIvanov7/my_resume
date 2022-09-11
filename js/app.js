21e3243b101dc6d0195e7cfb1f79800838d33424
Sam <sam1206morfey@gmail.com>
2022-09-11 23:07:18 +0300

    my_resume

---
+++ js/app.js
@@ -0,0 +1,18 @@
+VANTA.GLOBE({
+  el: "#page",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 800.00,
+  minWidth: 800.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x592a32,
+	color2: 0x15efff,
+  backgroundColor: 0x0,
+  points: 20.00,
+  maxDistance: 30.00,
+  spacing: 20.00,
+	size: 1.4,
+	size2: 3,
+})
